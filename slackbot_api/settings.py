249496382c7b911b7d381cf62dc581ec7c853959
DipdlavSingh <diplav@strongarmtech.com>
2020-10-07 01:30:55 +0530

    fix allowed host

--- slackbot_api/settings.py
+++ slackbot_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '5wfkye!%v3_@n_oweago3d-6+1=j2x)y*#7kbok@d9e4zpv)%0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://django-slack-bot.herokuapp.com/','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['django-slack-bot.herokuapp.com/','localhost', '127.0.0.1']
 
 
 # Application definition
