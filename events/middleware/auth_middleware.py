01a2cc4ecfe35649b0cad08c595362b69613ec8a
DipdlavSingh <diplav@strongarmtech.com>
2020-10-12 12:04:15 +0530

    minor change

--- events/middleware/auth_middleware.py
+++ events/middleware/auth_middleware.py
@@ -11,6 +11,8 @@ class AuthUser(object):
             if WebClient(user_token).api_call('auth.test')['ok']:
                 request.user_token = user_token
                 return None
+            else:
+                return JsonResponse({"message":"not authorised"}, status=403) # If user is not allowed raise Error
         except Exception as e:
             return JsonResponse({"message":str(e)}, status=403) # If user is not allowed raise Error
         return None
