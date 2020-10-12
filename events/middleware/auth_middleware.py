139f888cae60fdba1cb89982e981b26524d1e541
DipdlavSingh <diplav@strongarmtech.com>
2020-10-12 12:03:03 +0530

    add auth middleware

---
+++ events/middleware/auth_middleware.py
@@ -0,0 +1,16 @@
+from django.http import JsonResponse
+from slack import WebClient
+
+class AuthUser(object):
+    # Check if client is authenticated
+    def process_request(self, request):
+        user_token = request.data.get('user_token',
+            request.query_params.get('user_token',
+                request.headers.get('user_token', None)))
+        try:
+            if WebClient(user_token).api_call('auth.test')['ok']:
+                request.user_token = user_token
+                return None
+        except Exception as e:
+            return JsonResponse({"message":str(e)}, status=403) # If user is not allowed raise Error
+        return None
